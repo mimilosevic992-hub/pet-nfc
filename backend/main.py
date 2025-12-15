eb2a7062993b3be792927fff03304c4ff97b0b04
Milos Milosevic <mimilosevic992@gmail.com>
2025-12-15 23:02:44 +0100

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,10 @@ app = FastAPI(title="Pet NFC API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "https://pet-bmlywpedh-milos-projects-cf3a85f2.vercel.app",],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://pet-bmlywpedh-milos-projects-cf3a85f2.vercel.app",
+        ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
