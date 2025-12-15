9b744fa17efe8b567b880e0d373ff18502664398
Milos Milosevic <mimilosevic992@gmail.com>
2025-12-15 23:03:29 +0100

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://pet-bmlywpedh-milos-projects-cf3a85f2.vercel.app",
-        ]
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
